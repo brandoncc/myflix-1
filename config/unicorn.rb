a9f9a7253d0a3e7b48a0c4ded48cf5665374eef5
Walter <wxfwalter@gmail.com>
2017-11-23 08:39:01 +0800

    reconfig unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,9 +10,18 @@ before_fork do |server, worker|
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
+
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
 end
 
 after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+
   Sidekiq.configure_client do |config|
     config.redis = { :size => 1 }
   end
@@ -20,11 +29,4 @@ after_fork do |server, worker|
   Sidekiq.configure_server do |config|
     config.redis = { :size => 5 }
   end
-
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
 end
